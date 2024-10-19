3367d9fdb15247839407588353e19a4d678e7c26
ashrafkvt <ashrafkvt6197@gmail.com>
2024-10-20 05:04:40 +0530

    implement the image upload logic into s3

---
+++ reports/utils.py
@@ -0,0 +1,33 @@
+import boto3
+from botocore.exceptions import ClientError
+
+from patient_report.settings import (
+    AWS_ACCESS_KEY_ID, AWS_SECRET_ACCESS_KEY, AWS_S3_REGION_NAME,
+    AWS_STORAGE_BUCKET_NAME
+)
+
+
+def s3_client():
+    try:
+        s3 = boto3.client('s3',
+                          aws_access_key_id=AWS_ACCESS_KEY_ID,
+                          aws_secret_access_key=AWS_SECRET_ACCESS_KEY,
+                          region_name=AWS_S3_REGION_NAME)
+        return s3
+    except Exception as exception:
+        print(exception)
+    return None
+
+
+def create_presigned_url(object_name):
+    s3 = s3_client()
+    try:
+        response = s3.generate_presigned_url(
+            'get_object', Params={
+                'Bucket': AWS_STORAGE_BUCKET_NAME,
+                'Key': object_name}, ExpiresIn=600)
+    except ClientError as e:
+        print(e)
+        return None
+
+    return response
--- reports/views.py
+++ reports/views.py
@@ -8,8 +8,16 @@ from weasyprint import HTML
 
 from .serializer import PatientReportSerializer
 
+from .utils import s3_client, create_presigned_url
+
 
 class PatientReportView(APIView):
+    def generate_patient_report_filename(self, patient_report):
+        last_name = patient_report.patient_last_name
+        first_name = patient_report.patient_first_name
+        dob = patient_report.patient_dob
+        return f'CR_{last_name}_{first_name}_{dob}.pdf'
+
     def get(self, request):
         # Render the form template
         return render(request, 'reports/patient_form.html')
@@ -20,14 +28,18 @@ class PatientReportView(APIView):
         if serializer.is_valid():
             patient_report = serializer.save()
 
+            logo_url = create_presigned_url(
+                patient_report.clinic_logo_key)
             html_string = render_to_string(
-                'reports/report_template.html', {'report': patient_report})
+                'reports/report_template.html',
+                {'report': patient_report, 'logo': logo_url})
             pdf_file = HTML(string=html_string).write_pdf()
+            file_name = self.generate_patient_report_filename(patient_report)
 
             # Return the PDF as a downloadable file
             response = HttpResponse(pdf_file, content_type='application/pdf')
             response['Content-Disposition'] = \
-                f'attachment; filename="patient_report_{patient_report.id}.pdf"'
+                f'attachment; filename={file_name}'
             return response
 
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
